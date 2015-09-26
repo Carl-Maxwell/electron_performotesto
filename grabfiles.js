998b6994ada0dbf05345080e46a302f5898ac1da
Carl Maxwell <carl.maxwell@gmail.com>
2015-09-26 13:42:13 -0700

    initial commit

---
+++ grabfiles.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+
+function getFiles(base, allFiles) {
+  //allFiles = allFiles || [];
+
+  fs.readdir(base, function(err, files) {
+    files.forEach(function(file) {
+      fs.stat(base + '/' + file, function(err, stat) {
+        if (stat.isDirectory()) {
+          getFiles(base + "/" + file, allFiles);
+        } else {
+          allFiles.push(base + "/" + file);
+        }
+      });
+    });
+  });
+
+  return allFiles;
+}
+
+
+var x = [];
+
+getFiles('./testfiles/', x);
+
+var interval = setInterval(function() {
+  console.log(x);
+  if (x.length > 4) clearInterval(interval);
+}, 50);
