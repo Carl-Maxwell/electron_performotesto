78e33373235fb0dc47fcc5709fe9916b5672e1bf
Carl Maxwell <carl.maxwell@gmail.com>
2015-09-26 18:51:46 -0700

    better test folder/folder generation

--- generatetestfolder.py
+++ generatetestfolder.py
@@ -1,4 +1,4 @@
-import names
+from names import get_last_name
 from random import random, choice
 from os import utime, mkdir
 
@@ -6,21 +6,37 @@ class Generator:
     def __init__(self):
         self.files = []
         self.folders = ["./"]
+
         while len(self.folders) < 100:
             self.generate()
-        self.files.sort
+        self.files.sort()
         self.files = map(lambda x: x.replace('//', '/'), self.files)
+
+    def name(self, path):
+        return path + "/" + get_last_name().lower()
+
+    def isFileBad(self, file):
+        return file in self.files or file in self.folders
+
+    def touch(self, file):
+        if self.isFileBad(file):
+            return
+
+        self.files.append(file)
+        # utime(name)
+
+    def mkdir(self, file):
+        if self.isFileBad(file):
+            return
+
+        self.folders.append(file)
+        # mkdir(name)
 
     def generate(self):
         path = choice(self.folders)
-        name = names.get_last_name().lower()
 
-        if random() < 0.75:
-            # utime(path + name)
-            self.files.append(path + "/" + name)
-        else:
-            # mkdir(name)
-            self.folders.append(path + "/" + name)
+        self.touch(self.name(path))
+        self.mkdir(self.name(path))
 
 def main():
     g = Generator()
