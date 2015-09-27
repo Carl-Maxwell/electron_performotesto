b8605cfd84866212549ea0352f8df968db91268b
Carl Maxwell <carl.maxwell@gmail.com>
2015-09-26 19:46:45 -0700

    test script actually creates files & folders now
    * takes in the starting folder place as a argument
    * stripping weird double slash input
    * do no messing with files that already exist
    * swap utime for open (utime apparently does not create files)

--- generatetestfolder.py
+++ generatetestfolder.py
@@ -1,13 +1,19 @@
 from names import get_last_name
 from random import random, choice
 from os import utime, mkdir
+from os.path import exists
+
+import sys
 
 class Generator:
-    def __init__(self):
-        self.files = []
-        self.folders = ["./"]
+    def __init__(self, base = "./"):
+        if base[-1] == "/":
+            base = base[0:-1]
 
-        while len(self.folders) < 100:
+        self.files = []
+        self.folders = [base]
+
+        while len(self.files) < 100:
             self.generate()
         self.files.sort()
         self.files = map(lambda x: x.replace('//', '/'), self.files)
@@ -16,21 +22,21 @@ class Generator:
         return path + "/" + get_last_name().lower()
 
     def isFileBad(self, file):
-        return file in self.files or file in self.folders
+        return file in self.files or file in self.folders or exists(file)
 
     def touch(self, file):
         if self.isFileBad(file):
             return
 
         self.files.append(file)
-        # utime(name)
+        open(file, 'w')
 
     def mkdir(self, file):
         if self.isFileBad(file):
             return
 
         self.folders.append(file)
-        # mkdir(name)
+        mkdir(file)
 
     def generate(self):
         path = choice(self.folders)
@@ -38,10 +44,13 @@ class Generator:
         self.touch(self.name(path))
         self.mkdir(self.name(path))
 
-def main():
-    g = Generator()
+def main(base = "./"):
+    g = Generator(base)
     g.files.sort
     print("\n".join(g.files))
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        main()
